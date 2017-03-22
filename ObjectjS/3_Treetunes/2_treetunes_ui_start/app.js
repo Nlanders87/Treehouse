b6f8328703edb42190cadac2ce452ec9572ffefd
Nlanders87 <nlanders87@gmail.com>
2017-03-21 22:08:28 -0400

    Finished Object-Oriented JS Course

--- ObjectjS/3_Treetunes/2_treetunes_ui_start/app.js
+++
@@ -1,7 +0,0 @@
-var playlist = new Playlist();
-
-var hereComesTheSun = new Song("Here Comes the Sun", "The Beatles", "2:54");
-var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and the Waves", "3:43");
-
-playlist.add(hereComesTheSun);
-playlist.add(walkingOnSunshine);
