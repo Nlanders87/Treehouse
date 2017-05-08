aaad6c4d97ff244fe7036d7d9edcaf1aeb4cd161
Nlanders87 <nlanders87@gmail.com>
2017-05-08 17:54:17 -0400

    Finished first C# program

--- C#_Basics/Program.cs
+++ C#_Basics/Program.cs
@@ -5,20 +5,32 @@ namespace Treehouse
   class Program
   {
       static void Main()
-      {
-          // Prompt user for minutes exercised 
-          Console.Write("Enter how many minutes you exercised: "); 
+      {   
+          int runningTotal = 0;
+          bool keepGoing = true;
         
-          string entry = Console.ReadLine();
-        
-  
-        
-     
-          // Add minutes exercised to total 
-          // Display total minutes exercised to the screen 
-          Console.WriteLine("You've excercised " + entry + " minutes");
-          
-          // Repeat until user quits    
+          while (keepGoing) 
+          {
+                // Prompt user for minutes exercised 
+                Console.Write("Enter how many minutes you exercised or type ' quit' to exit: "); 
+                
+                string entry = Console.ReadLine();
+                
+                if(entry == "quit") 
+                {
+                    keepGoing = false;
+                } else 
+             {      
+                int minutes = int.Parse(entry);
+                runningTotal = runningTotal + minutes;
+                
+                // Add minutes exercised to total 
+                // Display total minutes exercised to the screen 
+                Console.WriteLine("You've excercised " + runningTotal + " minutes");
+                
+                // Repeat until user quits 
+              }
+         }
       }
    }
 }
