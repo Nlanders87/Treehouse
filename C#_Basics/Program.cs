5d3bc613185fbb6cff4f542c9c896dfdcdc9db78
Nlanders87 <nlanders87@gmail.com>
2017-05-30 23:18:05 -0400

    Added new features to first C# app

--- C#_Basics/Program.cs
+++ C#_Basics/Program.cs
@@ -6,29 +6,60 @@ namespace Treehouse
   {
       static void Main()
       {   
-          int runningTotal = 0;
+          double runningTotal = 0;
           bool keepGoing = true;
         
           while (keepGoing) 
           {
-                // Prompt user for minutes exercised 
-                Console.Write("Enter how many minutes you exercised or type ' quit' to exit: "); 
-                
-                string entry = Console.ReadLine();
-                
-                if(entry == "quit") 
+              // Prompt user for minutes exercised 
+              Console.Write("Enter how many minutes you exercised or type 'quit' to exit: "); 
+            
+              string entry = Console.ReadLine();
+            
+              if(entry.ToLower() == "quit") 
+              {
+                 break;
+              } else 
+              {
+              try 
                 {
-                    keepGoing = false;
-                } else 
-             {      
-                int minutes = int.Parse(entry);
-                runningTotal = runningTotal + minutes;
-                
-                // Add minutes exercised to total 
-                // Display total minutes exercised to the screen 
-                Console.WriteLine("You've excercised " + runningTotal + " minutes");
-                
-                // Repeat until user quits 
+                  double minutes = double.Parse(entry);
+                  
+                if(minutes <= 0)
+                {
+                  Console.WriteLine(minutes + " is not an acceptable response");
+                  continue;
+                }
+                else if(minutes <= 10)
+                {
+                  Console.WriteLine("Better than nothing, am I right?");
+                }
+                else if(minutes <= 30)
+                {
+                  Console.WriteLine("way to go hot stuff");
+                }
+                else if(minutes <= 60)
+                {
+                  Console.WriteLine("YOu must be a ninja warrior in training");
+                }
+                else
+                {
+                  Console.WriteLine("Okay, now you're just showing off");
+                }
+              runningTotal = runningTotal + minutes;
+                }
+                catch(FormatException)
+                {
+                  Console.WriteLine("That is not a valid input");
+                  continue;
+                }
+              
+            
+              // Add minutes exercised to total 
+              // Display total minutes exercised to the screen 
+              Console.WriteLine("You've excercised " + runningTotal + " minutes");
+              
+              // Repeat until user quits 
               }
          }
       }
