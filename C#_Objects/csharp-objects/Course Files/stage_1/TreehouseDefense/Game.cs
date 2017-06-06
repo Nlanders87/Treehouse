31a3d0b51d7aa1b2db39acfa18c721e94679292b
Nlanders87 <nlanders87@gmail.com>
2017-06-06 15:08:50 -0400

    C# game TreehouseDefense

---
+++ C#_Objects/csharp-objects/Course Files/stage_1/TreehouseDefense/Game.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TreehouseDefense
+{
+    class Game
+    {
+        public static void Main()
+        {
+            Map map = new Map(8,5);
+
+            MapLocation x = new MapLocation(4, 2);
+        }
+    }
+}
