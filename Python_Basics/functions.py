8047f7b0b0423dd9aac12addacc0cd7d2b2ff79b
Nlanders87 <nlanders87@gmail.com>
2017-09-11 20:05:46 -0400

    Deleted unnecessary files

--- Python_Basics/functions.py
+++ Python_Basics/functions.py
@@ -4,14 +4,14 @@ def hows_the_parrot():
 hows_the_parrot()
 
 def lumberjack(name, pronoun):
-        print("{}'s a lumberjack and {} OK!".format(name, pronoun))
+    print("{}'s a lumberjack and {} OK!".format(name, pronoun))
 
 lumberjack("Kenneth", "he's")
 lumberjack("Lacey", "she's")
 lumberjack("Sam", "they're")
 
 def average(num1, num2):
-    return (num1+num2) / 2
+    return(num1+num2) / 2
 
 avg = average(2, 8)
-print(avg)
+print(int(avg))
